0fa8d0cc64a2e579e73178f65bdc8ce159ebbf34
kingkuys2123 <kylematthew.quitco@cit.edu>
2026-03-16 12:00:04 +0800

    Initialize Lifewood project with Maven setup; add application properties and basic structure

---
+++ backend/src/main/java/com/lifewood/lifewood/LifewoodApplication.java
@@ -0,0 +1,13 @@
+package com.lifewood.lifewood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifewoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifewoodApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lifewood/lifewood/LifewoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifewood.lifewood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifewoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
